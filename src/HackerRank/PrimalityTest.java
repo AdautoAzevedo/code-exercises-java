90090cf668e4d9eafa9bf89e16bcb8dc29e9d63e
Adauto Azevêdo <adautofreitasdeazevedo@gmail.com>
2023-12-05 10:20:38 -0300

    Added a primality test exercise

---
+++ src/HackerRank/PrimalityTest.java
@@ -0,0 +1,24 @@
+package HackerRank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class PrimalityTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        BigInteger number = new BigInteger(n);
+        if (number.isProbablePrime(100)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+       
+        
+
+        bufferedReader.close();
+    }
+}
