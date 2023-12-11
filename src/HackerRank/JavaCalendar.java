c11e2c75758dec52c0a7ae54aaf20616e039a42e
Adauto Azevêdo <adautofreitasdeazevedo@gmail.com>
2023-12-11 16:00:47 -0300

    Added a java calendar exercise

---
+++ src/HackerRank/JavaCalendar.java
@@ -0,0 +1,44 @@
+package HackerRank;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Calendar;
+
+public class JavaCalendar {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+class Result {
+    public static String findDay(int month, int day, int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, (month-1), day);
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        
+        String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        String dayOfWeekString = daysOfWeek[dayOfWeek - 1];
+
+        return dayOfWeekString;
+    }
+
+}
