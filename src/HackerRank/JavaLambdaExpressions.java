0b1980a7b4bae43c1d655c966ba8ef70c2c0cfcf
Adauto Azevêdo <adautofreitasdeazevedo@gmail.com>
2023-12-22 14:47:11 -0300

    Added a Lambda expressions exercise

---
+++ src/HackerRank/JavaLambdaExpressions.java
@@ -0,0 +1,80 @@
+package HackerRank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class JavaLambdaExpressions {
+    public static void main(String[] args) throws IOException {
+        MyMath ob = new MyMath();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        PerformOperation op;
+        boolean ret = false;
+        String ans = null;
+        while (T--> 0) {
+            String s = br.readLine().trim();
+            StringTokenizer st = new StringTokenizer(s);
+            int ch = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            if (ch == 1) {
+                op = ob.isOdd();
+                ret = ob.checker(op, num);
+                ans = (ret) ? "ODD" : "EVEN";
+            } else if (ch == 2) {
+                op = ob.isPrime();
+                ret = ob.checker(op, num);
+                ans = (ret) ? "PRIME" : "COMPOSITE";
+            } else if (ch == 3) {
+                op = ob.isPalindrome();
+                ret = ob.checker(op, num);
+                ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+                }
+            System.out.println(ans);
+        }
+    }
+}
+
+interface PerformOperation {
+    boolean check(int a);
+}
+
+class MyMath {
+    public static boolean checker(PerformOperation p, int num) {
+        return p.check(num);
+    }
+
+    public PerformOperation isOdd() {
+        return num -> num % 2 != 0;
+    }
+
+    public PerformOperation isPrime() {
+        return num -> {
+            if (num <= 1) {
+                return false;
+            }
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        };
+    }
+
+    public PerformOperation isPalindrome() {
+        return num -> {
+            int original = num;
+            int reversed = 0;
+
+            while (num > 0) {
+                int digit = num % 10;
+                reversed = reversed * 10 + digit;
+                num /= 10;
+            }
+            return original == reversed;
+        };
+    }
+
+}
